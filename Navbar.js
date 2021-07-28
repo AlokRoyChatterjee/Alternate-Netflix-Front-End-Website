765adeb5acd8f06d47e8bc5a6ea3fae85d531a3f
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 20:27:18 -0400

    Update and rename Nav.js to Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Navbar.css";
+
+function Nav() {
+  return (
+    <div className="nav_black">
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
+        alt="logo"
+      />
+    </div>
+  )
+}
+
+export default Nav
