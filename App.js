c6cb980895c82e9812c1bb688a33c8f021c8b5c1
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 20:32:47 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import Row from './Row';
+import movies from './movies';
 import requests from './requests';
 
 function App() {
 	return (
 		<div className="app">
-			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-			<Row title="Trending" fetchUrl={requests.fetchTrending} />
-			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+			<movies title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+			<movies title="Trending" fetchUrl={requests.fetchTrending} />
+			<movies title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<movies title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<movies title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<movies title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<movies title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<movies title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 		</div>
 	);
 }
