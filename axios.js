84ccf682d455560c10de207089300a24f0403c83
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 20:31:27 -0400

    Update axios.js

--- axios.js
+++ axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-	baseURL: 'https://api.themoviedb.org/3'      //getting movie database info
+	url: 'https://api.themoviedb.org/3'      //getting movie info from database API
 });
 
 export default instance;
